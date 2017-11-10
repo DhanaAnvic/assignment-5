c6968d44cab3e99877627408cacde80bf1ab8bd8
David <David@192.168.2.63>
2017-11-10 19:17:38 -0400

    Finished

--- src/assignment5/IntNode1.java
+++ src/assignment5/IntNode1.java
@@ -19,15 +19,26 @@ public class IntNode1 {
         item = num;
         next = null;
     }
-    
+    /**
+     * Returns the number stored in the node
+     * @return the stored number
+     */
     public int getNum(){
         return item;
     }
     
+    /**
+     * Returns the node comes after
+     * @return the next node
+     */
     public IntNode1 getNext(){
         return next;
     }
     
+    /**
+     * Sets the node that comes after 
+     * @param node the node to point to
+     */
      public void setNext(IntNode1 node){
         next = node; 
 }
