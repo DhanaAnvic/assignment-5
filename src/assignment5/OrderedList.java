6eb8ebc9e2141e1b355fd3bbd4855b34ef3b5228
David <David@192.168.2.63>
2017-11-12 17:58:37 -0400

    Finished

--- src/assignment5/OrderedList.java
+++ src/assignment5/OrderedList.java
@@ -130,16 +130,11 @@ public class OrderedList {
          * @param args the command line arguments
          */
     public static void main(String[] args) {
-        OrderedList list = new OrderedList();
-        list.add(2);
-        list.add(-5);
-        list.remove(1);
-        for(int i = 0; i < list.size(); i++){
-            System.out.println(list.get(i));
 
 
-    }
     }
 }
+
+
    
 
